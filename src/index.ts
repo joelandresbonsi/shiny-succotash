8949ac92dfe7c860d97646e0c070f7b7762e77e6
Joel Bonsi <bonsimaxi@gmail.com>
2023-06-21 17:45:16 -0300

    cambios generales y creacion de assets

--- src/index.ts
+++ src/index.ts
@@ -1,20 +1,25 @@
-import { Application, Sprite } from 'pixi.js'
+import { Application, Assets, Sprite } from 'pixi.js'
 
 const app = new Application({
 	view: document.getElementById("pixi-canvas") as HTMLCanvasElement,
 	resolution: window.devicePixelRatio || 1,
 	autoDensity: true,
 	backgroundColor: 0x6495ed,
-	width: 640,
-	height: 480
+	width: 1024,
+	height: 1024
 });
 
-const clampy: Sprite = Sprite.from("clampy.png");
+Assets.add("Clampy", "./clampy.png");
+Assets.add("myHongo", "./hongo1.jpg");
 
+//para que esten las imagenes precargadas:
 
-clampy.anchor.set(0.5);
+Assets.load(["Clampy","myHongo"]).then(()=>{
 
-clampy.x = app.screen.width / 2;
-clampy.y = app.screen.height / 2;
+	const clampy: Sprite = Sprite.from("myHongo");
+	console.log ("Hola mundo!", clampy.width, clampy.height);
 
-app.stage.addChild(clampy);
+	app.stage.addChild(clampy);
+    clampy.x = 0;
+    clampy.y = 0;
+});
